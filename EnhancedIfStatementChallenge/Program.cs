bbfff4226c64523d49b5c4765e610d771c10d47c
Richard Tanner <ratanner0@gmail.com>
2022-07-13 09:37:57 -0400

    Add files via upload

---
+++ EnhancedIfStatementChallenge/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EnhancedIfStatementChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current temperature in Celcius.");
+            string tempInput = Console.ReadLine();
+
+            //test to make sure can convert the input to an int
+            bool converted = int.TryParse(tempInput, out int result); 
+            if (converted)
+            {
+                //if able to convert, displays the message
+                string tempMessage = result <= 15 ? "it is too cold here" : result > 28 ? "it is hot here" : "it is ok";
+                Console.WriteLine(tempMessage);
+            }
+            else
+            {
+                // displays if the input is not an int
+                Console.WriteLine("Not a valid Temperature.  Please enter a whole number.");
+            }
+
+            Console.Read();
+        }
+
+
+    }
+}
